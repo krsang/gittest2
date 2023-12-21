d755cc5cc2de5874d9ce5fbf96ed2bc6bc896e1f
USER <goodssh99@gmail.com>
2023-12-21 17:30:01 +0900

    First Commit

--- lastwork.py
+++ lastwork.py
@@ -2,6 +2,7 @@
 import socket
 import argparse
 from struct import pack
+import os
 
 DEFAULT_PORT = 69
 BLOCK_SIZE = 512
@@ -73,9 +74,39 @@ else:
     send_rrq(filename, mode, print_message=False)
 
 # Open a file with the same name to save data from the server
-file = open(filename, 'wb')
+file = None  # Initialize file to None
 expected_block_number = 1
 
+file_found_before_transfer = False
+
+# Flag to indicate if the file already exists locally
+file_exists_locally = os.path.exists(filename)
+
+# Open the file before sending the RRQ/WRQ message based on the operation
+try:
+    if operation == 'get':
+        if file_exists_locally:
+            file = open(filename, 'wb')  # Open the file in binary write mode
+            send_rrq(filename, mode, print_message=False)
+            file_found_before_transfer = True
+            print(f"File '{filename}' found on the server. Initiating transfer.")
+        else:
+            raise FileNotFoundError(f"File '{filename}' not found locally.")
+    elif operation == 'put':
+        if not file_exists_locally:
+            file = open(filename, 'xb')  # 'x' flag ensures that the file is created, raises FileExistsError if it already exists
+            send_wrq(filename, mode, print_message=False)
+            print(f"File '{filename}' created locally. Initiating transfer.")
+            file_found_before_transfer = True  # Set to True for 'put' operation
+        else:
+            raise FileExistsError(f"File '{filename}' already exists locally. Transfer aborted.")
+except FileNotFoundError as e:
+    print(e)
+except FileExistsError as e:
+    print(e)
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+file_found = False
 try:
     while True:
         # receive data from the server
@@ -91,21 +122,35 @@ try:
             if block_number == expected_block_number:
                 send_ack(block_number, server_new_socket, print_message=False)
                 file_block = data[4:]
+                if file is None:
+                    file = open(filename, 'wb')  # Open the file in binary write mode
                 file.write(file_block)
                 expected_block_number += 1
                 print(file_block.decode())
             else:
                 send_ack(block_number, server_new_socket, print_message=False)
 
+
         elif opcode == OPCODE['ERROR']:
             error_code = int.from_bytes(data[2:4], byteorder='big')
-            print(ERROR_CODE[error_code])
+            if error_code == 1 and file is not None:  # File not found (and the file was opened)
+                print(f"File '{filename}' not found on the server. Transfer aborted.")
+                file.close()
+                os.remove(filename)  # Remove the created file
+            else:
+                print(ERROR_CODE[error_code])
             break
 
         if len(file_block) < BLOCK_SIZE:
-            file.close()
-            print("File transfer successful.")  # Add this line for success
-            break
+            if file is not None:
+                file.close()
+                if file_block and operation == 'get':
+                    if file_found_before_transfer:
+                        print(f"File '{filename}' transfer successful.")
+                    else:
+                        print(f"File '{filename}' transfer incomplete.")
+                elif not file_found_before_transfer:
+                    print(f"File '{filename}' not found on the server.")
 except socket.timeout:
     print("Timeout during data transfer")
 
